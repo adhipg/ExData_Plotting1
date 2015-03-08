60a251d80c993b3caac0b51afaa598a74feb35f2
Adhip Gupta <me@adhipg.in>
2015-03-08 19:01:39 +0530

    Add plot1.R and plot1.png

---
+++ plot1.R
@@ -0,0 +1,19 @@
+filePath <- 'household_power_consumption.txt'
+
+# helps in the development.
+if( !exists("rawData") ) {
+  rawData <- read.table(filePath, sep=";", header=TRUE, na.strings="?")
+}
+
+# Add in a date/time row and subset to get the data that we are interested in
+fromDate = as.Date('2007/02/01', format='%Y/%m/%d')
+toDate = as.Date('2007/02/02', format='%Y/%m/%d')
+householdData <- subset(rawData, as.Date(rawData$Date, format='%d/%m/%Y') >= fromDate &
+                          as.Date(rawData$Date, format='%d/%m/%Y') <= toDate)
+householdData$DateTime <- strptime(paste(householdData$Date, householdData$Time), '%d/%m/%Y %H:%M')
+
+png("plot1.png", width=480, height=480)
+
+hist(householdData$Global_active_power, col="red", xlab="Global Active Power (kilowatts)",
+     main="Global Active Power")
+dev.off()
